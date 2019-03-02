9c5db870425f75ad4312955a37772952cbbd0abf
Espinoza-Carlos <esp16013@byui.edu>
2019-03-01 20:21:14 -0800

    updated4

---
+++ documents/topic-12/transforms.js
@@ -0,0 +1,19 @@
+// CREATE
+function newElement(tag) {return document.createElement(tag);  }
+function newText(text)   {return document.createTextNode(text);}
+function newEvent(id, event, action) {getByID(id).addEventListener(event, action);}
+
+// DESTROY
+function delElement(parent, child) {getByID(parent).removeChild(child);}
+function delEvent(id, event, action) {getByID(id).removeEventListener(event, action);}
+
+// GET ELEMENTS
+function getByID(id)        {return document.getElementById(id);          }
+function getByClass(name)   {return document.getElementsByClassName(name);}
+function getByTag(name)     {return document.getElementsByTagName(name);  }
+function getBySelector(css) {return document.querySelectorAll(css);       }
+
+// GET FORM DATA
+function getInput(formName, inputName) {
+  return document.forms.formName.inputName.value;
+}
