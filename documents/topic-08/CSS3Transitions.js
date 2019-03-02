e2e00cf366456cbc1a5d6d1a77e76db188c2ad76
Espinoza-Carlos <esp16013@byui.edu>
2019-03-01 22:47:14 -0800

    updated6

---
+++ documents/topic-08/CSS3Transitions.js
@@ -0,0 +1,29 @@
+one.onclick = function() {
+	changeStoplight();
+	var loop = setInterval(changeStoplight, 10000);
+}
+var doc = document;
+function changeStoplight(){
+	
+	doc.getElementById("one").style.background = "red";
+	doc.getElementById("three").style.background = "black";
+	doc.getElementById("rocket").className = "";
+
+	setTimeout(function(){ 
+		doc.getElementById("one").style.background = "black";
+		doc.getElementById("two").style.background = "yellow";
+	}, 3000);
+
+	setTimeout(function(){ 
+		doc.getElementById("two").style.background = "black";
+		doc.getElementById("three").style.background = "green";
+		doc.getElementById("rocket").className = "move";
+	}, 6000);
+}
+
+loadButton.onclick = function() {
+	loader.className = "increase";
+	setTimeout(function(){ 
+		loadedImg.style.display = "block";
+	}, 7000);
+}
